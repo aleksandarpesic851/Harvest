d6f9a02bc9e7acbf9246e20ced9f5cb8a2528683
Sudipta Biswas <13803232+biswassudipta05@users.noreply.github.com>
2021-03-03 22:10:38 +0530

    Added Validation for feedback form

--- feedback/add.php
+++ feedback/add.php
@@ -3,10 +3,20 @@
     require_once $_SERVER['DOCUMENT_ROOT'] . "/enable_error_report.php";
 
     if(isset($_POST["comment"])) {
-        $feedback = $_POST["comment"];
+        $feedback = validate_input($_POST["comment"]);
         $query = "INSERT INTO `feedbacks` (`feedback`, `created_at`) VALUES ('$feedback', now())";
         mysqli_query($conn, $query);
         mysqli_close($conn);
         echo "ok";
     }
+
+    function validate_input($data) {
+        $regex = "/[^A-Za-z0-9.,!@#$%_&*()? ]+/";
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = preg_replace($regex, '', $data);
+        $data = htmlspecialchars($data);
+        return $data;
+      }
+    
 ?>
