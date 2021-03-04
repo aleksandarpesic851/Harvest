a9d5ade81798e94930efadb1e7cb8d1825c38a88
Sudipta Biswas <13803232+biswassudipta05@users.noreply.github.com>
2021-03-05 02:14:54 +0530

    Update feedback validation
    Added validation to check for empty value and truncate value if required from serverside

--- feedback/add.php
+++ feedback/add.php
@@ -2,7 +2,7 @@
     require_once $_SERVER['DOCUMENT_ROOT'] . "/db_connect.php";
     require_once $_SERVER['DOCUMENT_ROOT'] . "/enable_error_report.php";
 
-    if(isset($_POST["comment"])) {
+    if(!empty($_POST["comment"])) {
         $feedback = validate_input($_POST["comment"]);
         $query = "INSERT INTO `feedbacks` (`feedback`, `created_at`) VALUES ('$feedback', now())";
         mysqli_query($conn, $query);
@@ -10,8 +10,9 @@
         echo "ok";
     }
 
-    function validate_input($data) {
+    function validate_input($data) {       
         $regex = "/[^A-Za-z0-9.,!@#$%_&*()? ]+/";
+        $data = substr($data,0,250);
         $data = trim($data);
         $data = stripslashes($data);
         $data = preg_replace($regex, '', $data);
