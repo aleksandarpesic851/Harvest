1ebc5cafb8ae40ff1d2368167fccf455308b0425
Sudipta Biswas <13803232+biswassudipta05@users.noreply.github.com>
2021-03-06 00:18:00 +0530

    Added Validation for feedback form (#15)
    * Added Validation for feedback form
    
    * Update feedback validation
    
    Added validation to check for empty value and truncate value if required from serverside
    
    * Added POST validation for feedback data
    
    * Removed extra if
    
    * Removed extra curly braces

--- feedback/add.php
+++ feedback/add.php
@@ -2,11 +2,22 @@
     require_once $_SERVER['DOCUMENT_ROOT'] . "/db_connect.php";
     require_once $_SERVER['DOCUMENT_ROOT'] . "/enable_error_report.php";
 
-    if(isset($_POST["comment"])) {
-        $feedback = $_POST["comment"];
+    if(($_SERVER["REQUEST_METHOD"] == "POST") && !empty($_POST["comment"])) {
+        $feedback = validate_input($_POST["comment"]);
         $query = "INSERT INTO `feedbacks` (`feedback`, `created_at`) VALUES ('$feedback', now())";
         mysqli_query($conn, $query);
         mysqli_close($conn);
         echo "ok";
     }
+
+    function validate_input($data) {       
+        $regex = "/[^A-Za-z0-9.,!@#$%_&*()? ]+/";
+        $data = substr($data,0,250);
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = preg_replace($regex, '', $data);
+        $data = htmlspecialchars($data);
+        return $data;
+      }
+    
 ?>
